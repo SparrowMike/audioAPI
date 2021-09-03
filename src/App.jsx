c3997d6b8e5d0917317e85f162e50ff0eba19e5b
sparrowmike <m.sp4rrow@gmai.com>
2021-09-03 20:23:34 +0800

    feat: 🎸 added filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.scss";
+import Filter from "./components/Filter";
 import Osc1 from "./components/Osc1";
 
 const actx = new AudioContext();
@@ -7,15 +8,25 @@ const out = actx.destination;
 
 const osc1 = actx.createOscillator();
 const gain1 = actx.createGain();
+const filter = actx.createBiquadFilter();
 
 osc1.connect(gain1);
-gain1.connect(out);
+gain1.connect(filter);
+filter.connect(out);
 
 function App() {
   const [osc1Settings, setOsc1Settings] = useState({
     frequency: osc1.frequency.value,
     detune: osc1.detune.value,
     type: osc1.type,
+  });
+
+  const [filterSettings, setFilterSettings] = useState({
+    frequency: filter.frequency.value,
+    detune: filter.detune.value,
+    Q: filter.Q.value,
+    gain: filter.gain.value,
+    type: filter.type,
   });
 
   const changeOsc1 = (e) => {
@@ -30,15 +41,34 @@ function App() {
     osc1.type = id;
   };
 
+  const changeFilter = (e) => {
+    let { value, id } = e.target;
+    setFilterSettings({ ...filterSettings, [id]: value });
+    filter[id].value = value;
+  };
+
+  const changeFilterType = (e) => {
+    let { id } = e.target;
+    setFilterSettings({ ...filterSettings, type: id });
+    filter.type = id;
+  };
+
   return (
     <div className="App">
-      <h1>Sliders</h1>
-      <button onClick={() => osc1.start()}>Noise</button>
-      <button onClick={() => osc1.stop()}>Stop</button>
+      <div className="center">
+        <h1>Sliders</h1>
+        <button onClick={() => osc1.start()}>Noise</button>
+        <button onClick={() => osc1.stop()}>Stop</button>
+      </div>
       <Osc1
         change={changeOsc1}
         settings={osc1Settings}
         changeType={changeOsc1Type}
+      />
+      <Filter
+        change={changeFilter}
+        settings={filterSettings}
+        changeType={changeFilterType}
       />
     </div>
   );
